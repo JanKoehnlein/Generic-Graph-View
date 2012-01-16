c4d98753620b94809d1efa014cf10bb616d262c2
Jan Koehnlein <jan.koehnlein@itemis.de>
2012-01-16 14:58:13 +0100

    minor model changes

--- org.eclipse.xtext.graphview.example/src-gen/company/Employee.java
+++ org.eclipse.xtext.graphview.example/src-gen/company/Employee.java
@@ -1,6 +1,16 @@
 package company;
 
+import company.Boss;
 import company.Person;
 
 public class Employee extends Person {
+  private Boss boss;
+  
+  public Boss getBoss() {
+    return this.boss;
+  }
+  
+  public void setBoss(final Boss boss) {
+    this.boss = boss;
+  }
 }
